f9b8b1ec10bec21cf9173818c89b2d7a83b83409
Oleg Konstantinov <oleg.boss@gmail.com>
2025-10-04 09:21:28 +0400

    Created protocol Printable

---
+++ Lession10/Protocols/Printable.swift
@@ -0,0 +1,16 @@
+//
+//  Printable.swift
+//  Lession10
+//
+//  Created by Oleg Konstantinov on 04.10.2025.
+//
+
+import Foundation
+
+/*
+ 6 - Протокол "Printable"
+ Определи протокол с методом printInfo().
+ */
+protocol Printable {
+    func printInfo()
+}
