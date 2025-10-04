11645f0f91a5a8cb1dae5f4dd3ae390badd8fb5a
Oleg Konstantinov <oleg.boss@gmail.com>
2025-10-04 09:05:17 +0400

    Create protocol Calculatable

---
+++ Lession10/Protocols/Calculatable.swift
@@ -0,0 +1,17 @@
+//
+//  Calculateble.swift
+//  Lession10
+//
+//  Created by Oleg Konstantinov on 04.10.2025.
+//
+
+import Foundation
+
+/*
+ 5 - Протокол "Calculatable"
+ Определи протокол с методом calculate(a:b:) -> Int.
+ */
+
+protocol Calculatable {
+    func calculate(a: Int, b: Int) -> Int
+}
